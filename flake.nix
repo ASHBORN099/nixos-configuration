4c3449eeaf2be297668db345e3a0858f11043706
Harsh Chawla <harshrcc@gmail.com>
2026-04-05 17:35:12 +0530

    feat: add home-manager to flake inputs

--- flake.nix
+++ flake.nix
@@ -2,19 +2,27 @@
   description = "ASHBORN099's Professional NixOS Flake";
 
   inputs = {
-    # We use the 'unstable' branch for the latest AI/Python packages
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    # WE ARE ADDING THIS LINE:
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
-    # This 'default' name must match the one in your install.sh
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
     nixosConfigurations.default = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = { inherit inputs; };
       modules = [
         ./configuration.nix
-        # The installer will automatically create the hardware-configuration.nix
-        ./hosts/default/hardware-configuration.nix 
+        ./hosts/default/hardware-configuration.nix
+        # AND THIS LINE TO CONNECT HOME-MANAGER:
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          # Change 'tester' to your username if you aren't using the script's default
+          home-manager.users.tester = import ./home.nix;
+        }
       ];
     };
   };
